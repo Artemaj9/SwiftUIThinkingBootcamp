bba67dbfe8ca39f47de9b55e02784176aea671fb
Artem <moiseev@physics.msu.ru>
2023-06-17 15:28:59 +0300

    Initial Commit

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingBootcampApp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Artem on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
