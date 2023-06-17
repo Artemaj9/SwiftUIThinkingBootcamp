e1e260c4b6ac6e4003406653d632e84ef2238a83
Artem <moiseev@physics.msu.ru>
2023-06-17 23:27:10 +0300

    ExtractSubviews

---
+++ ExtractSubviewsBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ExtractSubviewsBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Artem on 17.06.2023.
+//
+
+import SwiftUI
+
+struct ExtractSubviewsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExtractSubviewsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtractSubviewsBootcamp()
+    }
+}
