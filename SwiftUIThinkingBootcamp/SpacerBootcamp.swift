56716345adb542333c6c95884046275552df1601
Artem <moiseev@physics.msu.ru>
2023-06-17 20:28:49 +0300

    Inits and enums

--- SwiftUIThinkingBootcamp/SpacerBootcamp.swift
+++ SwiftUIThinkingBootcamp/SpacerBootcamp.swift
@@ -9,7 +9,57 @@ import SwiftUI
 
 struct SpacerBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    /*    HStack (spacing: 0) {
+            Spacer(minLength: 0)
+                .frame(height: 10)
+                .background(Color.orange)
+            Rectangle()
+                .frame(width: 50, height: 50)
+            Spacer()
+                .frame(height: 10)
+                .background(Color.orange)
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 50, height: 50)
+            Spacer()
+                .frame(height: 10)
+                .background(Color.orange)
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 50, height: 50)
+            Spacer(minLength: 0)
+                .frame(height: 10)
+                .background(Color.orange)
+            
+        }
+        .background(Color.yellow)
+    //  .padding(.horizontal, 200)
+  //    .background(Color.blue)
+     */
+        
+        
+        VStack {
+            HStack(spacing: 0) {
+                Image(systemName: "xmark")
+                Spacer()
+                    .frame(height: 10)
+                    .background(Color.orange)
+                Image(systemName: "gear")
+                  
+            }
+            .font(.title)
+//            .background(Color.yellow)
+            .padding(.horizontal)
+      //  .background(Color.blue)
+            
+            Spacer()
+                .frame(width: 10)
+                .background(Color.orange)
+            Rectangle()
+                .frame(height:55)
+        }
+        .padding()
+        .background(Color.yellow)
     }
 }
 
