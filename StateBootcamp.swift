6871cdb639a2e494673e0259b25089b4e11e4df5
Artem <moiseev@physics.msu.ru>
2023-06-17 23:12:49 +0300

    @State property

--- StateBootcamp.swift
+++ StateBootcamp.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct StateBootcamp: View {
+    
+    @State var backgroundColor: Color = Color.green
+    @State var myTitle: String = "My Title"
+    @State var count: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //background
+           backgroundColor
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 20) {
+                Text(myTitle)
+                    .font(.title)
+                Text("Count: \(count)")
+                    .font(.headline)
+                    .underline()
+                
+                HStack(spacing: 29) {
+                    Button("Button 1") {
+                        backgroundColor = .red
+                        myTitle = "BUTTON 1 was pressed"
+                        count += 1
+                    }
+                    
+                    Button("Button 2") {
+                        backgroundColor = .purple
+                        myTitle = "BUTTON 2 was pressed"
+                        count += 1
+                    }
+                }
+                
+                Button("Resume") {
+                    backgroundColor = .green
+                    myTitle = "My Title"
+                    count = 0
+                }
+                
+            }
+            .foregroundColor(.white)
+        }
     }
 }
 
