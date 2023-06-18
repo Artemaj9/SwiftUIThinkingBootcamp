40369273feee598669fe06a2307f25eb831b2191
Artem <moiseev@physics.msu.ru>
2023-06-18 10:48:15 +0300

    Binding property wrapper

--- ExtractSubviewsBootcamp.swift
+++ ExtractSubviewsBootcamp.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct ExtractSubviewsBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.cyan.ignoresSafeArea()
+          
+            contentLayer
+        }
+        
+    }
+    var contentLayer: some View {
+        HStack(spacing:20) {
+            MyItem(title: "Oranges", count: 5, color: .orange)
+            MyItem(title: "Apples", count: 9, color: .red)
+            MyItem(title: "Bananas", count: 7, color: .yellow)
+        }
     }
 }
-
 struct ExtractSubviewsBootcamp_Previews: PreviewProvider {
     static var previews: some View {
         ExtractSubviewsBootcamp()
+    }
+}
+
+struct MyItem: View {
+    let title: String
+    let count: Int
+    let color: Color
+    
+    var body: some View {
+        VStack {
+            Text("\(count)")
+            Text(title)
+        }
+        .padding(20)
+        .background(color)
+        .cornerRadius(20)
     }
 }
