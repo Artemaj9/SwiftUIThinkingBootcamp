721a2ea8f8ab91d2f6bb57599779489a4e0bbcae
Artem <moiseev@physics.msu.ru>
2023-06-19 18:00:37 +0300

    Color Picker

--- SwiftUIThinkingBootcamp/PickerBootcamp.swift
+++ SwiftUIThinkingBootcamp/PickerBootcamp.swift
@@ -11,7 +11,38 @@ struct PickerBootcamp: View {
     
     @State var selectedNumber: Int = 0
     @State var selection: String = "1"
+    @State var filterOptions: [String] = [
+    "Msot Recent", "Most Popular", "Most Liked"
+    ]
+    
+    init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor.tintColor
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor : UIColor.white
+        ]
+        UISegmentedControl.appearance().setTitleTextAttributes(attributes, for: .selected)
+    }
+    
     var body: some View {
+        
+        
+        Picker (
+            selection: $selection,
+            label: Text("Picker"),
+                content: {
+            ForEach(filterOptions.indices) {
+                index in
+                Text(filterOptions[index])
+                    .tag(filterOptions[index])
+            }
+        })
+        .pickerStyle(.segmented)
+        .background(Color.yellow)
+        
+        
+        
+        
+        
         
         Menu {
             Picker(selection: $selectedNumber, label: EmptyView()) {
@@ -24,6 +55,26 @@ struct PickerBootcamp: View {
         }
         
     }
+    
+    
+    var customLabel: some View {
+        HStack {
+            Image(systemName: "paperplane")
+            Text(String(selectedNumber))
+            Spacer()
+            Text("\u{2335}")
+                .offset(y:  -5)
+        }
+        .foregroundColor(.white)
+        .font(.title)
+        .padding()
+        .frame(height: 32)
+        .background(Color.blue)
+        .cornerRadius(16)
+        .shadow(color: .purple, radius: 10, x: 1, y: 1)
+        .offset(y: 10)
+    }
+}
     
     //        Picker(
     //            selection: .constant(1),
@@ -54,23 +105,7 @@ struct PickerBootcamp: View {
     //            .pickerStyle()
     //        }
     
-    
-    var customLabel: some View {
-        HStack {
-            Image(systemName: "paperplane")
-            Text(String(selectedNumber))
-            Spacer()
-            Text("\u{2335}")
-                .offset(y:  -5)
-        }
-        .foregroundColor(.white)
-        .font(.title)
-        .padding()
-        .frame(height: 32)
-        .background(Color.blue)
-        .cornerRadius(16)
-    }
-}
+
 
 struct PickerBootcamp_Previews: PreviewProvider {
     static var previews: some View {
