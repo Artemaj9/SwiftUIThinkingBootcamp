86ff6fa0c7f75722853990aeabc2b28806456ab2
Artem <moiseev@physics.msu.ru>
2023-06-21 02:03:03 +0300

    @FocusState bootcamp

---
+++ SwiftUIThinkingBootcamp/FocusStateBootcamp.swift
@@ -0,0 +1,66 @@
+//
+//  FocusStateBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Artem on 21.06.2023.
+//
+
+import SwiftUI
+
+struct FocusStateBootcamp: View {
+    
+    @FocusState private var usernameInFocus: Bool
+    @State private var username: String = ""
+    @FocusState private var passwordInFocus: Bool
+    @State private var password: String = ""
+    var body: some View {
+        VStack {
+            TextField("Add your name here", text: $username)
+                .focused($usernameInFocus)
+                .padding(.leading)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.brightness(0.3))
+            
+            
+            TextField("Add your password here", text: $password)
+                .focused($passwordInFocus)
+                .padding(.leading)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.brightness(0.3))
+            
+            
+            Button("SIGN UP ✨") {
+                let usernameIsValid = !username.isEmpty
+                let passwordIsValid = !password.isEmpty
+                if usernameIsValid && passwordIsValid {
+                    print("SIGN UP")
+                } else if usernameIsValid {
+                    usernameInFocus = false
+                    passwordInFocus = true
+                } else {
+                    usernameInFocus = true
+                    passwordInFocus = false
+                }
+            Button("TOGGLE FOCUS STATE") {
+                passwordInFocus.toggle()
+            }
+            //You can use it like a submit button
+            .padding(40)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.usernameInFocus = true
+                }
+            }
+            
+        }
+        .padding(40)
+    }
+}
+
+struct FocusStateBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FocusStateBootcamp()
+    }
+}
