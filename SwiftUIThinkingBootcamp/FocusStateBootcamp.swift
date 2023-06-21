e0be41905077791b259db36b2ee2df1d5d58681f
Artem <moiseev@physics.msu.ru>
2023-06-21 20:34:52 +0300

    NavigationStack bootcamp

--- SwiftUIThinkingBootcamp/FocusStateBootcamp.swift
+++ SwiftUIThinkingBootcamp/FocusStateBootcamp.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct FocusStateBootcamp: View {
     
-    @FocusState private var usernameInFocus: Bool
+    enum OnboardingFields: Hashable {
+        case username
+        case password
+    }
+    
+  //  @FocusState private var usernameInFocus: Bool
     @State private var username: String = ""
-    @FocusState private var passwordInFocus: Bool
+   // @FocusState private var passwordInFocus: Bool
     @State private var password: String = ""
+    @FocusState private var fieldInFocus: OnboardingFields?
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             TextField("Add your name here", text: $username)
-                .focused($usernameInFocus)
+                .focused($fieldInFocus, equals: .username)
                 .padding(.leading)
                 .frame(height: 55)
                 .frame(maxWidth: .infinity)
@@ -24,7 +30,7 @@ struct FocusStateBootcamp: View {
             
             
             TextField("Add your password here", text: $password)
-                .focused($passwordInFocus)
+                .focused($fieldInFocus, equals: .password)
                 .padding(.leading)
                 .frame(height: 55)
                 .frame(maxWidth: .infinity)
@@ -37,27 +43,30 @@ struct FocusStateBootcamp: View {
                 if usernameIsValid && passwordIsValid {
                     print("SIGN UP")
                 } else if usernameIsValid {
-                    usernameInFocus = false
-                    passwordInFocus = true
+                    fieldInFocus = .password
+//                    usernameInFocus = false
+//                    passwordInFocus = true
                 } else {
-                    usernameInFocus = true
-                    passwordInFocus = false
+                    fieldInFocus = .username
+//                    usernameInFocus = true
+//                    passwordInFocus = false
                 }
-            Button("TOGGLE FOCUS STATE") {
-                passwordInFocus.toggle()
             }
+//            Button("TOGGLE FOCUS STATE") {
+//                passwordInFocus.toggle()
+//            }
             //You can use it like a submit button
             .padding(40)
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    self.usernameInFocus = true
+//            .onAppear {
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//                    self.usernameInFocus = true
                 }
             }
             
         }
-        .padding(40)
-    }
-}
+//        .padding(40)
+    
+
 
 struct FocusStateBootcamp_Previews: PreviewProvider {
     static var previews: some View {
